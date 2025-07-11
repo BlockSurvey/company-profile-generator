fd9ad70163b15436aa6cd475f61934033d44eff1
Raja Ilayaperumal <rajailayaperumal@Rajas-MacBook-Pro.local>
2025-07-11 09:47:47 +0530

    setup: New component is added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,9 @@
-import { useBearStore } from '@/stores/zustand.store';
+import CompanyProfile from './components/company-profile';
 
 export default function Home() {
-  const { bears, increase, reset } = useBearStore();
-
-  const returnSubComponent = () => {
-    return <>Bears {bears}</>;
-  };
-
   return (
     <>
-      <h1>Zustand</h1>
-      Bears {bears}
-      <button
-        type="button"
-        onClick={() => {
-          increase(1);
-        }}
-      >
-        Increase
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          reset();
-        }}
-      >
-        Reset
-      </button>
+      <CompanyProfile />
     </>
   );
 }
